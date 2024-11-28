c67ff7def92bb65213ac6b449468c16892bf8ca5
Dom8558 <ghaytadakshubham387@gmail.com>
2024-11-28 14:50:23 +0530

    added important feature VVVVip

--- ff.py
+++ ff.py
@@ -4,9 +4,11 @@ my_list = []
 my_list.append(10)
 my_list.append(20)
 my_list.append(30)
+my_list.append(100)
 
 # Print the updated list
 print("Updated list:", my_list)
 print("hii")
 print("hey sunil love you")
 print("love you too ashih")
+
