181e0df565bb6ed1281223a6a63547479f3b262d
Dom8558 <ghaytadakshubham387@gmail.com>
2024-11-28 13:48:38 +0530

    added important feature VVVVip

---
+++ ff.py
@@ -0,0 +1,10 @@
+my_list = []
+
+# Append some elements to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+
+# Print the updated list
+print("Updated list:", my_list)
+print("h
